dc1422c82eb5b28abc5464bb4e6b08dca07e7a87
Dominique d'Argent <dargent@in.tum.de>
2016-03-13 20:40:54 +0100

    Precompute colors
    Performance boost: 50%

--- Mandelsemmel/MandelbrotView.m
+++ Mandelsemmel/MandelbrotView.m
@@ -8,8 +8,53 @@
 
 #import "MandelbrotView.h"
 
+@interface MandelbrotView ()
+
+@property (nonatomic) NSUInteger maxIterations;
+@property (nonatomic, strong) NSArray<UIColor *> *colors;
+
+@end
 
 @implementation MandelbrotView
+
+- (instancetype)initWithCoder:(NSCoder *)aDecoder {
+    self = [super initWithCoder:aDecoder];
+    
+    if (self) {
+        [self commonInit];
+    }
+    
+    return self;
+}
+
+- (instancetype)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    
+    if (self) {
+        [self commonInit];
+    }
+    
+    return self;
+}
+
+- (void)commonInit {
+    self.maxIterations = 255;
+    [self precomputeColors];
+}
+
+- (void)precomputeColors {
+    NSMutableArray *colors = [NSMutableArray arrayWithCapacity:self.maxIterations + 1];
+    
+    for (int i = 0; i < self.maxIterations; ++i) {
+        colors[i] = [UIColor colorWithHue:(CGFloat)i/self.maxIterations
+                               saturation:1.0
+                               brightness:1.0
+                                    alpha:1.0];
+    }
+    colors[self.maxIterations] = [UIColor blackColor];
+    
+    self.colors = colors;
+}
 
 - (void)drawRect:(CGRect)rect {
     NSLog(@"drawRect: %@", NSStringFromCGRect(rect));
@@ -39,7 +84,9 @@
         for (int screenX = CGRectGetMinX(screenRect); screenX < CGRectGetMaxX(screenRect); ++screenX) {
             CGFloat cX = CGRectGetMinX(viewport) + (screenX + 0.5) * pixelWidth;
             
-            UIColor *color = [self colorForPoint:CGPointMake(cX, cY)];
+            NSUInteger iterations = [self calculateNumberOfMandelbrotIterationsForPoint:CGPointMake(cX, cY)];
+            
+            UIColor *color = self.colors[iterations];
             [color setFill];
             
             CGRect pixel = CGRectMake(screenX / scale, screenY / scale, 1, 1);
@@ -47,23 +94,10 @@
             CGContextFillRect(context, pixel);
         }
     }
-    NSLog(@"end drawing");
-    
-}
-
-- (UIColor *)colorForPoint:(CGPoint)c {
-    const NSUInteger MaxIterations = 255;
-    NSUInteger iterations = [self calculateNumberOfMandelbrotIterationsForPoint:c withMaxIterations:MaxIterations];
-    
-    if (iterations == MaxIterations) {
-        return [UIColor blackColor];
-    }
-    
-    return [UIColor colorWithHue:(CGFloat)iterations/MaxIterations saturation:1.0 brightness:1.0 alpha:1.0];
     NSLog(@"end drawing, took: %f sec", [[NSDate date] timeIntervalSinceDate:start]);
 }
 
-- (NSUInteger)calculateNumberOfMandelbrotIterationsForPoint:(CGPoint)c withMaxIterations:(NSUInteger)maxIterations {
+- (NSUInteger)calculateNumberOfMandelbrotIterationsForPoint:(CGPoint)c {
     NSUInteger iteration;
     
     const int EscapeRadius = 2;
@@ -72,7 +106,7 @@
     CGPoint z = CGPointZero;
     CGPoint z2 = CGPointZero;
     
-    for (iteration = 0; (z2.x + z2.y) < ER2 && iteration < maxIterations; ++iteration) {
+    for (iteration = 0; (z2.x + z2.y) < ER2 && iteration < self.maxIterations; ++iteration) {
         z.y = 2 * z.x * z.y + c.y;
         z.x = z2.x - z2.y + c.x;
         
