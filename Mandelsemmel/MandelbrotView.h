c00a6b204e92557afeba0c92aa3e2147e9ba5e45
Dominique d'Argent <dargent@in.tum.de>
2016-03-13 17:16:09 +0100

    Add first, naïve implementation
    No zooming, bad performance.

---
+++ Mandelsemmel/MandelbrotView.h
@@ -0,0 +1,13 @@
+//
+//  MandelbrotView.h
+//  Mandelsemmel
+//
+//  Created by Dominique d'Argent on 13/03/16.
+//  Copyright © 2016 Dominique d'Argent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MandelbrotView : UIView
+
+@end
---
+++ Mandelsemmel/MandelbrotView.m
@@ -0,0 +1,77 @@
+//
+//  MandelbrotView.m
+//  Mandelsemmel
+//
+//  Created by Dominique d'Argent on 13/03/16.
+//  Copyright © 2016 Dominique d'Argent. All rights reserved.
+//
+
+#import "MandelbrotView.h"
+
+
+@implementation MandelbrotView
+
+- (void)drawRect:(CGRect)rect {
+    NSLog(@"drawRect: %@", NSStringFromCGRect(rect));
+    
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGAffineTransform transform = CGContextGetCTM(context);
+    CGFloat scale = transform.a;
+
+    NSLog(@"scale: %f", scale);
+    
+    CGRect screenRect = CGRectMake(rect.origin.x * scale,
+                                   rect.origin.y * scale,
+                                   rect.size.width * scale,
+                                   rect.size.height * scale);
+    
+    CGRect viewport = CGRectMake(-2.5, -2.0, 4.0, 4.0);
+    
+    CGFloat pixelWidth = (CGRectGetMaxX(viewport) - CGRectGetMinX(viewport))  / (CGRectGetMaxX(screenRect) - CGRectGetMinX(screenRect));
+    CGFloat pixelHeight = (CGRectGetMaxY(viewport) - CGRectGetMinY(viewport)) / (CGRectGetMaxY(screenRect) - CGRectGetMinY(screenRect));
+    
+    NSLog(@"begin drawing");
+    for (int screenY = CGRectGetMinY(screenRect); screenY < CGRectGetMaxY(screenRect); ++screenY) {
+        CGFloat cY = CGRectGetMinY(viewport) + (screenY + 0.5) * pixelHeight;
+        
+        for (int screenX = CGRectGetMinX(screenRect); screenX < CGRectGetMaxX(screenRect); ++screenX) {
+            CGFloat cX = CGRectGetMinX(viewport) + (screenX + 0.5) * pixelWidth;
+            
+            UIColor *color = [self colorForPoint:CGPointMake(cX, cY)];
+            [color setFill];
+            
+            CGRect pixel = CGRectMake(screenX / scale, screenY / scale, 1, 1);
+            
+            CGContextFillRect(context, pixel);
+        }
+    }
+    NSLog(@"end drawing");
+    
+}
+
+- (UIColor *)colorForPoint:(CGPoint)c {
+    const int MaxIterations = 255;
+    const int EscapeRadius = 2;
+    const int ER2 = EscapeRadius * EscapeRadius;
+    
+    int iteration;
+    
+    CGPoint z = CGPointZero;
+    CGPoint z2 = CGPointZero;
+    
+    for (iteration = 0; (z2.x + z2.y) < ER2 && iteration < MaxIterations; ++iteration) {
+        z.y = 2 * z.x * z.y + c.y;
+        z.x = z2.x - z2.y + c.x;
+        
+        z2.x = z.x * z.x;
+        z2.y = z.y * z.y;
+    }
+    
+    if (iteration == MaxIterations) {
+        return [UIColor blackColor];
+    }
+    
+    return [UIColor colorWithHue:(CGFloat)iteration/MaxIterations saturation:1.0 brightness:1.0 alpha:1.0];
+}
+
+@end
